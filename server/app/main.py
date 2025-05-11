67e5f49c2a5c3ec8a08a700b9adba4674de31246
hyewon6588 <hhan29@my.centennialcollege.ca>
2025-05-12 03:13:19 +0900

    migrated server to tokyo

--- server/app/main.py
+++ server/app/main.py
@@ -10,7 +10,7 @@ app.include_router(notification.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://well-done-today.vercel.app/"],
+    allow_origins=["http://localhost:3000","https://well-done-today.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
