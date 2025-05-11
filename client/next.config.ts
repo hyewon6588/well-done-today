6aab2df2280373df1aab4b7209e3d5aa972731c4
hyewon6588 <hhan29@my.centennialcollege.ca>
2025-05-11 23:34:20 +0900

    fix2

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.resolve.alias['@'] = require('path').resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 export default nextConfig;
