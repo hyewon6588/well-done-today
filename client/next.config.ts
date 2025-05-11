5af2064a456eee0cef995896bc5f4e9e14105e5d
hyewon6588 <hhan29@my.centennialcollege.ca>
2025-05-11 23:45:51 +0900

    fix4

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,15 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   webpack: (config) => {
-    config.resolve.alias['@'] = require('path').resolve(__dirname, 'src');
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src"),
+    };
+    config.resolve.extensions?.push(".ts", ".tsx");
+    config.resolve.modules = [path.resolve(__dirname), "node_modules"];
     return config;
   },
 };
